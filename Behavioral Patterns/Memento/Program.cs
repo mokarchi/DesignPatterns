20840b95786933b04990f546f4b72c13ab277f2a
Amir <mokarchi@gmail.com>
2020-11-27 01:35:36 +0330

    Add project files.

---
+++ Behavioral Patterns/Memento/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Memento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
