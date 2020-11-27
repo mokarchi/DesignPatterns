b11ba135a6c1336637d9f2d059fbd6f93479b540
Amir <mokarchi@gmail.com>
2020-11-27 22:55:09 +0330

    Real World with Solution 4

--- Behavioral Patterns/Memento/Program.cs
+++ Behavioral Patterns/Memento/Program.cs
@@ -1,4 +1,5 @@
-﻿using Memento.Solution_3;
+﻿using Memento.Real_world_with_Solution_4;
+using Memento.Solution_3;
 using Memento.Solution_4;
 using System;
 
@@ -67,6 +68,24 @@ namespace Memento
             //o.SetMemento(c.Memento);
             //// Wait for user
             //Console.ReadKey();
+            #endregion
+
+            #region Real World with Solution 4
+            SalesProspect s = new SalesProspect();
+            s.Name = "Noel van Halen";
+            s.Phone = "(412) 256-0990";
+            s.Budget = 25000.0;
+            // Store internal state
+            ProspectMemory m = new ProspectMemory();
+            m.Memento = s.SaveMemento();
+            // Continue changing originator
+            s.Name = "Leo Welch";
+            s.Phone = "(310) 209-7111";
+            s.Budget = 1000000.0;
+            // Restore saved state
+            s.RestoreMemento(m.Memento);
+            // Wait for user
+            Console.ReadKey();
             #endregion
         }
     }
---
+++ Behavioral Patterns/Memento/Real-world with Solution 4/ProspectMemory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento.Real_world_with_Solution_4
+{
+    /// <summary>
+    /// The 'Caretaker' class
+    /// </summary>
+    public class ProspectMemory
+    {
+        private Memento _memento;
+
+        public Memento Memento
+        {
+            set { _memento = value; }
+            get { return _memento; }
+        }
+    }
+}
