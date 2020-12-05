f7bee886051b410cedf62a9294999cf480e13b20
Amir <mokarchi@gmail.com>
2020-12-05 15:28:08 +0330

    State Pattern

--- Behavioral Patterns/State/Program.cs
+++ Behavioral Patterns/State/Program.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using State.Solution_1;
-using State.StateScheme;
-using StatePattern;
+﻿using StatePattern;
 
 namespace State
 {
@@ -9,17 +6,6 @@ namespace State
     {
         static void Main(string[] args)
         {
-            //var mp3Player = new MP3PlayerContext();
-            //mp3Player.Play();
-            //Console.WriteLine(string.Format("The MP3 player state is {0}.", mp3Player.CurrentState));
-            //mp3Player.Play();
-            //Console.WriteLine(string.Format("The MP3 player state is {0}.", mp3Player.CurrentState));
-
-            // The client code.
-            //var context = new Context(new ConcreteStateA());
-            //context.Request1();
-            //context.Request2();
-
             var gumballmachine = new GumballMachine(5);
             gumballmachine.InsertQuarter();
             gumballmachine.TurnCrank();
