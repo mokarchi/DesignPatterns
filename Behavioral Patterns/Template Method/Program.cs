0f54526b0ddbc3fd8d2d8b9f4cda5243478a6c9c
Amir <mokarchi@gmail.com>
2020-12-11 12:00:52 +0330

    Template Method

---
+++ Behavioral Patterns/Template Method/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Template_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sourdough sourdough = new Sourdough();
+            sourdough.Make();
+
+            TwelveGrain twelveGrain = new TwelveGrain();
+            twelveGrain.Make();
+
+            WholeWheat wholeWheat = new WholeWheat();
+            wholeWheat.Make();
+        }
+    }
+}
---
+++ Behavioral Patterns/Template Method/TwelveGrain.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Template_Method
+{
+    class TwelveGrain : Bread
+    {
+        public override void MixIngredients()
+        {
+            Console.WriteLine("Gathering Ingredients for 12-Grain Bread.");
+        }
+
+        public override void Bake()
+        {
+            Console.WriteLine("Baking the 12-Grain Bread. (25 minutes)");
+        }
+    }
+}
