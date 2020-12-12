5b3ab0cfbe0f515f077f09b6951e73cb533bdf9e
Amir <mokarchi@gmail.com>
2020-12-12 11:49:45 +0330

    eshop example with command pattern

--- Behavioral Patterns/Command/Program.cs
+++ Behavioral Patterns/Command/Program.cs
@@ -1,4 +1,5 @@
-﻿using Command.restaurant;
+﻿using Command.eshop;
+using Command.restaurant;
 using System;
 
 namespace Command
@@ -7,36 +8,55 @@ namespace Command
     {
         static void Main(string[] args)
         {
-            Patron patron = new Patron();
-            patron.SetCommand(1 /*Add*/);
-            patron.SetMenuItem(new MenuItem("French Fries", 2, 1.99));
-            patron.ExecuteCommand();
+            #region restaurant
+            //Patron patron = new Patron();
+            //patron.SetCommand(1 /*Add*/);
+            //patron.SetMenuItem(new MenuItem("French Fries", 2, 1.99));
+            //patron.ExecuteCommand();
 
-            patron.SetCommand(1 /*Add*/);
-            patron.SetMenuItem(new MenuItem("Hamburger", 2, 2.59));
-            patron.ExecuteCommand();
+            //patron.SetCommand(1 /*Add*/);
+            //patron.SetMenuItem(new MenuItem("Hamburger", 2, 2.59));
+            //patron.ExecuteCommand();
 
-            patron.SetCommand(1 /*Add*/);
-            patron.SetMenuItem(new MenuItem("Drink", 2, 1.19));
-            patron.ExecuteCommand();
+            //patron.SetCommand(1 /*Add*/);
+            //patron.SetMenuItem(new MenuItem("Drink", 2, 1.19));
+            //patron.ExecuteCommand();
 
-            patron.ShowCurrentOrder();
+            //patron.ShowCurrentOrder();
 
-            //Remove the french fries
-            patron.SetCommand(3 /*Remove*/);
-            patron.SetMenuItem(new MenuItem("French Fries", 2, 1.99));
-            patron.ExecuteCommand();
+            ////Remove the french fries
+            //patron.SetCommand(3 /*Remove*/);
+            //patron.SetMenuItem(new MenuItem("French Fries", 2, 1.99));
+            //patron.ExecuteCommand();
 
-            patron.ShowCurrentOrder();
+            //patron.ShowCurrentOrder();
 
-            //Now we want 4 hamburgers rather than 2
-            patron.SetCommand(2 /*Edit*/);
-            patron.SetMenuItem(new MenuItem("Hamburger", 4, 2.59));
-            patron.ExecuteCommand();
+            ////Now we want 4 hamburgers rather than 2
+            //patron.SetCommand(2 /*Edit*/);
+            //patron.SetMenuItem(new MenuItem("Hamburger", 4, 2.59));
+            //patron.ExecuteCommand();
 
-            patron.ShowCurrentOrder();
+            //patron.ShowCurrentOrder();
 
-            Console.ReadKey();
+            //Console.ReadKey();
+            #endregion
+
+            var modifyPrice = new ModifyPrice();
+            var product = new Product("Phone", 500);
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Increase, 100));
+
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Increase, 50));
+            Execute(product, modifyPrice, new ProductCommand(product, PriceAction.Decrease, 2500));
+            Console.WriteLine(product);
+            Console.WriteLine();
+            modifyPrice.UndoActions();
+            Console.WriteLine(product);
+        }
+
+        private static void Execute(Product product, ModifyPrice modifyPrice, ICommand productCommand)
+        {
+            modifyPrice.SetCommand(productCommand);
+            modifyPrice.Invoke();
         }
     }
 }
---
+++ Behavioral Patterns/Command/eshop/ICommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Command.eshop
+{
+    public interface ICommand
+    {
+        void ExecuteAction();
+        void UndoAction();
+    }
+}
