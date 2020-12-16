06072e61bfee8a8fe1cbd2560fc1e8d80f66ae6b
Amir <mokarchi@gmail.com>
2020-12-16 11:33:18 +0330

    implement Composite Pattern with example of drawing

---
+++ Structural/Composite/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Composite.Drawing;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a tree structure 
+            CompositeElement root = new CompositeElement("Picture");
+            root.Add(new PrimitiveElement("Red Line"));
+            root.Add(new PrimitiveElement("Blue Circle"));
+            root.Add(new PrimitiveElement("Green Box"));
+
+            // Create a branch
+            CompositeElement comp = new CompositeElement("Two Circles");
+            comp.Add(new PrimitiveElement("Black Circle"));
+            comp.Add(new PrimitiveElement("White Circle"));
+            root.Add(comp);
+
+            // Add and remove a PrimitiveElement
+            PrimitiveElement pe = new PrimitiveElement("Yellow Line");
+            root.Add(pe);
+            root.Remove(pe);
+
+            // Recursively display nodes
+            root.Display(1);
+        }
+    }
+}
