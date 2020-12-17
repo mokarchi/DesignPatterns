9ecd2672516c0674e0e0416493a10e2a5d860ead
Amir <mokarchi@gmail.com>
2020-12-17 18:48:01 +0330

    implement Decorator Pattern with example of restaurant

---
+++ Structural/Decorator/Restaurant/FreshSalad.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Restaurant
+{
+    /// <summary>
+    /// A ConcreteComponent class
+    /// </summary>
+    class FreshSalad : RestaurantDish
+    {
+        private string _greens;
+        private string _cheese; //I am going to use this pun everywhere I can
+        private string _dressing;
+
+        public FreshSalad(string greens, string cheese, string dressing)
+        {
+            _greens = greens;
+            _cheese = cheese;
+            _dressing = dressing;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("\nFresh Salad:");
+            Console.WriteLine(" Greens: {0}", _greens);
+            Console.WriteLine(" Cheese: {0}", _cheese);
+            Console.WriteLine(" Dressing: {0}", _dressing);
+        }
+    }
+}
---
+++ Structural/Decorator/Restaurant/RestaurantDish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Restaurant
+{
+    /// <summary>
+    /// The abstract Component class
+    /// </summary>
+    abstract class RestaurantDish
+    {
+        public abstract void Display();
+    }
+}
