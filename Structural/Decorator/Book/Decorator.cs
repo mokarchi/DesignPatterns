eb23ea48a0dd29b53e953fbc0d83ffa135668e7c
Amir <mokarchi@gmail.com>
2020-12-17 18:42:17 +0330

    implement Decorator Pattern with example of book

---
+++ Structural/Decorator/Book/Decorator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Book
+{
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+    abstract class Decorator : LibraryItem
+    {
+        protected LibraryItem libraryItem;
+
+        public Decorator(LibraryItem libraryItem)
+        {
+            this.libraryItem = libraryItem;
+        }
+
+        public override void Display()
+        {
+            libraryItem.Display();
+        }
+    }
+}
---
+++ Structural/Decorator/Program.cs
@@ -0,0 +1,25 @@
+﻿using Decorator.Book;
+using System;
+
+namespace Decorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create book
+            Decorator.Book.Book book = new Decorator.Book.Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+
+            // Create video
+            Video video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+            // Make video borrowable, then borrow and display
+            Console.WriteLine("\nMaking video borrowable:");
+            Borrowable borrowvideo = new Borrowable(video);
+            borrowvideo.BorrowItem("Customer #1");
+            borrowvideo.BorrowItem("Customer #2");
+            borrowvideo.Display();
+        }
+    }
+}
