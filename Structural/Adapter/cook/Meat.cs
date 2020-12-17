6a7baafc803815a76a3305f072dd9f8a2651fc1c
Amir <mokarchi@gmail.com>
2020-12-17 17:59:21 +0330

    Adapter Pattern with cook example

---
+++ Structural/Adapter/cook/Meat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.cook
+{
+    /// <summary>
+    /// The new Meat class, which represents 
+    /// details about a particular kind of meat.
+    /// </summary>
+    class Meat
+    {
+        protected string MeatName;
+        protected float SafeCookTempFahrenheit;
+        protected float SafeCookTempCelsius;
+        protected double CaloriesPerOunce;
+        protected double ProteinPerOunce;
+
+        // Constructor
+        public Meat(string meat)
+        {
+            this.MeatName = meat;
+        }
+
+        public virtual void LoadData()
+        {
+            Console.WriteLine("\nMeat: {0} ------ ", MeatName);
+        }
+    }
+}
---
+++ Structural/Adapter/cook/MeatDatabase.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Adapter.cook
+{
+    /// <summary>
+    /// The legacy API which must be converted to the new structure
+    /// </summary>
+    class MeatDatabase
+    {
+        // Temps from http://www.foodsafety.gov/keep/charts/mintemp.html
+        public float GetSafeCookTemp(string meat, TemperatureType tempType)
+        {
+            if (tempType == TemperatureType.Fahrenheit)
+            {
+                switch (meat)
+                {
+                    case "beef":
+                    case "pork":
+                        return 145f;
+
+                    case "chicken":
+                    case "turkey":
+                        return 165f;
+
+                    default:
+                        return 165f;
+                }
+            }
+            else
+            {
+                switch (meat)
+                {
+                    case "beef":
+                    case "veal":
+                    case "pork":
+                        return 63f;
+
+                    case "chicken":
+                    case "turkey":
+                        return 74f;
+
+                    default:
+                        return 74f;
+                }
+            }
+        }
+
+        public int GetCaloriesPerOunce(string meat)
+        {
+            switch (meat.ToLower())
+            {
+                case "beef": return 71;
+                case "pork": return 69;
+                case "chicken": return 66;
+                case "turkey": return 38; //Wow, turkey is lean!
+                default: return 0;
+            }
+        }
+
+        public double GetProteinPerOunce(string meat)
+        {
+            switch (meat.ToLower())
+            {
+                case "beef": return 7.33f;
+                case "pork": return 7.67f;
+                case "chicken": return 8.57f;
+                case "turkey": return 8.5f;
+                default: return 0d;
+            }
+        }
+    }
+}
