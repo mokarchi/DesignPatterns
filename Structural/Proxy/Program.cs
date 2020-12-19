0fc974bdc98695da99ae8e60bc4ebd2a1ea15240
Amir <mokarchi@gmail.com>
2020-12-19 20:01:42 +0330

    Implementation of Proxy Pattern

--- Structural/Proxy/Program.cs
+++ Structural/Proxy/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Proxy.Math;
+using Proxy.restaurant;
 
 namespace Proxy
 {
@@ -8,11 +9,22 @@ namespace Proxy
         static void Main(string[] args)
         {
             // Create math proxy
-            MathProxy proxy = new MathProxy();
-            Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
-            Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
-            Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
-            Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+            //MathProxy proxy = new MathProxy();
+            //Console.WriteLine("4 + 2 = " + proxy.Add(4, 2));
+            //Console.WriteLine("4 - 2 = " + proxy.Sub(4, 2));
+            //Console.WriteLine("4 * 2 = " + proxy.Mul(4, 2));
+            //Console.WriteLine("4 / 2 = " + proxy.Div(4, 2));
+
+            NewServerProxy proxy = new NewServerProxy();
+
+            //Manage orders from a table
+            Console.WriteLine("What would you like to order? ");
+            string order = Console.ReadLine();
+            proxy.TakeOrder(order);
+            Console.WriteLine("Sure thing!  Here's your " + proxy.DeliverOrder() + ".");
+            Console.WriteLine("How would you like to pay?");
+            string payment = Console.ReadLine();
+            proxy.ProcessPayment(payment);
         }
     }
 }
