8bdccdb25f864ad97bf3d8c81e85a3f3a35efe8e
Amir <mokarchi@gmail.com>
2020-12-19 18:47:47 +0330

    Implementation of Flyweight Pattern

---
+++ Structural/Flyweight/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Flyweight.characters;
+
+namespace Flyweight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build a document with text
+            //string document = "AAZZBBZB";
+            //char[] chars = document.ToCharArray();
+            //CharacterFactory factory = new CharacterFactory();
+
+            //// extrinsic state
+            //int pointSize = 10;
+            //foreach (char c in chars)
+            //{
+            //    pointSize++;
+            //    Character character = factory.GetCharacter(c);
+            //    character.Display(pointSize);
+            //}
+
+
+        }
+    }
+}
---
+++ Structural/Flyweight/characters/Character.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight.characters
+{
+    /// <summary>
+    /// The 'Flyweight' abstract class
+    /// </summary>
+    abstract class Character
+    {
+        protected char symbol;
+        protected int width;
+        protected int height;
+        protected int ascent;
+        protected int descent;
+        protected int pointSize;
+        public abstract void Display(int pointSize);
+    }
+}
