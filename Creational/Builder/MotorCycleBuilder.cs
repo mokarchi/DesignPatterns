26d8a7975cc011f64ecf8e0b86056ae3b42137d4
Amir <mokarchi@gmail.com>
2020-12-21 11:09:20 +0330

    Builder

---
+++ Creational/Builder/MotorCycleBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    /// <summary>
+    /// The 'ConcreteBuilder1' class
+    /// </summary>
+    class MotorCycleBuilder : VehicleBuilder
+    {
+        public MotorCycleBuilder()
+        {
+            vehicle = new Vehicle("MotorCycle");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "MotorCycle Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "500 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+    }
+}
---
+++ Creational/Builder/Shop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Builder
+{
+    /// <summary>
+    /// The 'Director' class
+    /// </summary>
+    class Shop
+    {
+        public void Construct(VehicleBuilder vehicleBuilder)
+        {
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
+        }
+    }
+}
