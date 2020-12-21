7bb7c0f39863b11b1bb03b12e91d641dc89f17a2
Amir <mokarchi@gmail.com>
2020-12-21 11:13:08 +0330

    Prototype

---
+++ Creational/Prototype/ColorPrototype.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    /// <summary>
+    /// The 'Prototype' abstract class
+    /// </summary>
+    abstract class ColorPrototype
+    {
+        public abstract ColorPrototype Clone();
+    }
+}
