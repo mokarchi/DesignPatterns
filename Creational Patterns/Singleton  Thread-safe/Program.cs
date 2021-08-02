cccc446c5e14f6dc2ae68126f7491d79b7ddb98c
mokarchi <mokarchi@gmail.com>
2021-08-02 22:39:32 +0430

    edit directory

---
+++ Creational Patterns/Singleton  Thread-safe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton__Thread_safe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Creational Patterns/Singleton No Thread-Safe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Singleton_No_Thread_Safe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
