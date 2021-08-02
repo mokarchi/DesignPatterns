dae040c51332d9a3b8b6166c17b7f4bbf81eda16
mokarchi <mokarchi@gmail.com>
2021-08-02 22:46:02 +0430

    add file

---
+++ Creational Patterns/AbstractFactory/Abstract Products/Animal.cs
@@ -0,0 +1,7 @@
+﻿namespace AbstractFactory
+{
+    public interface Animal
+    {
+        string speak();
+    }
+}
---
+++ Creational Patterns/AbstractFactory/Abstract Products/AnimalFactory.cs
@@ -0,0 +1,15 @@
+﻿namespace AbstractFactory
+{
+    public abstract class AnimalFactory
+    {
+        public abstract Animal GetAnimal(string AnimalType);
+
+        public static AnimalFactory CreateAnimalFactory(string FactoryType)
+        {
+            if (FactoryType.Equals("Sea"))
+                return new SeaAnimalFactory();
+            else
+                return new LandAnimalFactory();
+        }
+    }
+}
