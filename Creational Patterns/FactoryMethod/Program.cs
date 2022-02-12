aa9b9ad00dc49b2128dbf484babb1119a6487b90
mokarchi <mokarchi@gmail.com>
2022-02-12 15:40:25 +0330

    clean program

--- Creational Patterns/FactoryMethod/Program.cs
+++ Creational Patterns/FactoryMethod/Program.cs
@@ -1,33 +1,24 @@
-﻿using System;
+﻿using FactoryMethod;
 
-namespace FactoryMethod
+CardFactory factory = null;
+Console.Write("Enter the card type you would like to visit: ");
+string car = Console.ReadLine();
+switch (car.ToLower())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            CardFactory factory = null;
-            Console.Write("Enter the card type you would like to visit: ");
-            string car = Console.ReadLine();
-            switch (car.ToLower())
-            {
-                case "moneyback":
-                    factory = new MoneyBackFactory(50000, 0);
-                    break;
-                case "titanium":
-                    factory = new TitaniumFactory(100000, 500);
-                    break;
-                case "platinum":
-                    factory = new PlatinumFactory(500000, 1000);
-                    break;
-                default:
-                    break;
-            }
-            CreditCard creditCard = factory.CreateProduct();
-            Console.WriteLine("\nYour card details are below : \n");
-            Console.WriteLine("Card Type: {0}\nCredit Limit: {1}\nAnnual Charge: {2}",
-                creditCard.CardType, creditCard.CreditLimit, creditCard.AnnualCharge);
-            Console.ReadKey();
-        }
-    }
+    case "moneyback":
+        factory = new MoneyBackFactory(50000, 0);
+        break;
+    case "titanium":
+        factory = new TitaniumFactory(100000, 500);
+        break;
+    case "platinum":
+        factory = new PlatinumFactory(500000, 1000);
+        break;
+    default:
+        break;
 }
+CreditCard creditCard = factory.CreateProduct();
+Console.WriteLine("\nYour card details are below : \n");
+Console.WriteLine("Card Type: {0}\nCredit Limit: {1}\nAnnual Charge: {2}",
+    creditCard.CardType, creditCard.CreditLimit, creditCard.AnnualCharge);
+Console.ReadKey();
